c338d1e5150a0afd2ed5dfc527ef81508edb4dea
jesgarrio <Pasaje10>
2020-02-20 08:58:41 +0100

    Added Mongo Client

--- python/GoogleCall.py
+++ python/GoogleCall.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from pymongo import MongoClient, errors
 import os
 
-port = 27017
-host = os.getenv('MONGO_INITDB_HOST', 'mongodb') #This is the alias in Docker
-username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
-password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
-database = os.getenv('MONGO_INITDB_DATABASE')
-# use a try-except indentation to catch MongoClient() errors
-try:
-    # try to instantiate a client instance
-    client = MongoClient(
-        host = [ str(host) + ":" + str(port) ],
-        serverSelectionTimeoutMS = 3000, # 3 second timeout
-        username = str(username),
-        password = str(password),
-    )
 
-    # print the version of MongoDB server if connection successful
-    print ("server version:", client.server_info()["version"])
+class MongoConnection(object):
+    _client = None
+    _port = 27017
+    _host = os.getenv('MONGO_INITDB_HOST', 'mongodb')  # This is the alias in Docker
+    _username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    _password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+    _database = os.getenv('MONGO_INITDB_DATABASE')
 
-    # get the database_names from the MongoClient()
-    database_names = client.list_database_names()
+    def __init__(self):
+        try:
+            self.client = MongoClient(
+                host=[str(self._host) + ":" + str(self._port)],
+                serverSelectionTimeoutMS=3000,  # 3 second timeout
+                username = str(self._username),
+                password = str(self._password)
+            )
+        except errors.ServerSelectionTimeoutError as err:
+            print("pymongo ERROR:", err)
 
-except errors.ServerSelectionTimeoutError as err:
-    # set the client and DB name list to 'None' and `[]` if exception
-    client = None
-    database_names = []
+    @property
+    def client(self):
+        return self._client
 
-    # catch pymongo.errors.ServerSelectionTimeoutError
-    print ("pymongo ERROR:", err)
+    @client.setter
+    def client(self, value):
+        self._client = value
 
+connection = MongoConnection()
+database_names = connection.client.list_database_names()
 print ("\ndatabases:", database_names)
