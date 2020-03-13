03c5f6e45164c08243f9700a2236983c5fc5bee4
jesgarrio <Pasaje10>
2020-03-13 01:10:46 +0100

    max length changed

--- django_server/place_info/models.py
+++ django_server/place_info/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class PlaceInfo(models.Model):
-    website = models.CharField(max_length=200, null=True)
+    website = models.CharField(max_length=500, null=True)
     name = models.CharField(max_length=150, null=True)
     rating = models.DecimalField(null=True, decimal_places=2, max_digits=3)
     international_phone_number = models.CharField(max_length=30, null=True)
